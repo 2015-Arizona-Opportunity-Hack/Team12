40639243514e0f5562221522e0c4b686f073ea0e
sindhuri <sindhurigajula@gmail.com>
2015-10-10 21:43:31 -0700

    register user

--- milkserver/routes/user.js
+++ milkserver/routes/user.js
@@ -9,7 +9,7 @@ router.post('/', function(req, res) {
     var results = [];
 
     // Grab data from http request
-    var data = {username: req.body.username, password: req.body.password, interests: req.body.interests, address: req.body.address, phone: req.body.phone};
+    var data = {firstname: req.body.firstname,lastname: req.body.lastname, emailid: req.body.emailid,phone: req.body.phone,dob: req.body.dob, addressline1: req.body.addressline1,  addressline2: req.body.addressline2, city: req.body.city, state: req.body.state,zip: req.body.zip, password: req.body.password};
 
     // Get a Postgres client from the connection pool
     pg.connect(connectionString, function(err, client, done) {
@@ -21,21 +21,21 @@ router.post('/', function(req, res) {
         }
 
         // SQL Query > Insert Data
-        client.query("INSERT INTO user2(username, password, interests, address, phone) values($1, $2, $3, $4, $5)", [data.username, data.password, data.interests, data.address, data.phone]);
+        client.query("INSERT INTO users(firstname,lastname, emailid, phone,dob,addressline1,addressline2,city,state,zip,password) values($1, $2, $3, $4, $5,$6, $7, $8, $9, $10,$11)", [data.firstname,data.lastname,data.emailid,data.phone, data.dob, data.addressline1, data.addressline2,data.city,data.state,data.zip, data.password]);
 
         // SQL Query > Select Data
-        var query = client.query("SELECT * FROM user2 ORDER BY id ASC");
+       // var query = client.query("SELECT * FROM user2 ORDER BY id ASC");
 
         // Stream results back one row at a time
-        query.on('row', function(row) {
+       /* query.on('row', function(row) {
             results.push(row);
-        });
+        });*/
 
         // After all data is returned, close connection and return results
-        query.on('end', function() {
-            done();
-            return res.json(results);
-        });
+      //  query.on('end', function() {
+            //done();
+        return res.json({ message: 'You registered successfully!' });
+       // });
 
 
     });
