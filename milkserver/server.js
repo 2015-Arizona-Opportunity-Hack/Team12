768817ad1726862258495bba6c9213ffcd33b8ad
ravi <ravikanth.valiveti@gmail.com>
2015-10-11 05:08:33 -0700

    cors issues

--- milkserver/server.js
+++ milkserver/server.js
@@ -13,6 +13,10 @@ var usereventRoute = require('./routes/userevents');
 var eventRoute = require('./routes/event');
 // configure app to use bodyParser()
 // this will let us get the data from a POST
+//Resolve cors prob ionic
+var cors = require('cors');
+app.use(cors());
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
