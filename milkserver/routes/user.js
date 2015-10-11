332f46f76e035d8fca55e04617d268884f04c956
sindhuri <sindhurigajula@gmail.com>
2015-10-11 06:31:06 -0700

    user profile

--- milkserver/routes/user.js
+++ milkserver/routes/user.js
@@ -28,7 +28,7 @@ router.post('/', function(req, res) {
        // client.query("INSERT INTO userinterests(emailid,user_interests) values ($1,$2)",[data.emailid])
        
         // SQL Query > Select Data
-       // var query = client.query("SELECT * FROM user2 ORDER BY id ASC");
+       //var query = client.query("SELECT * FROM users ORDER BY id ASC");
 
         // Stream results back one row at a time
        /* query.on('row', function(row) {
@@ -43,6 +43,37 @@ router.post('/', function(req, res) {
 
 
     });
+});
+router.get('/', function(req, res) {
+
+    var results = [];
+    var getemailid=req.query.emailid;
+
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+        // Handle connection errors
+        if(err) {
+          done();
+          console.log(err);
+          return res.status(500).json({ success: false, data: err});
+        }
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM users where emailid=($1)",[getemailid]);
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return res.json(results);
+        });
+
+    });
+
 });
 
 module.exports = router;
